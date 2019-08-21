8136a0676063faeacb310c889c62e3d5fc0fd542
Tyler Small <intrasolarmusic@gmail.com>
2019-08-21 12:46:54 -0500

    Qa (#4)
    * Intial Commit (rails new --api)
    
    * Use pg and jsonbuilder
    
    * Update to postgres for QA to build
    
    * Add auth controller and routes
    
    * Add the skeleton for a login route
    
    * Make login controller a pass-thru for the params
    
    * Bundle install and changed the gemlock
    
    * Add the initial draft of the Open API Json Schema
    
    * Update the version of the Open API schema
    
    * Change the auth controller
    
    * Bump nokogiri from 1.10.3 to 1.10.4 (#2)
    
    * Qa merge to deploy to prod (#1)
    
    * Intial Commit (rails new --api)
    
    * Use pg and jsonbuilder
    
    * Update to postgres for QA to build
    
    * Add auth controller and routes
    
    * Add the skeleton for a login route
    
    * Make login controller a pass-thru for the params
    
    * Bundle install and changed the gemlock
    
    * Add the initial draft of the Open API Json Schema
    
    * Update the version of the Open API schema
    
    * Change the auth controller
    
    * Bump nokogiri from 1.10.3 to 1.10.4
    
    Bumps [nokogiri](https://github.com/sparklemotion/nokogiri) from 1.10.3 to 1.10.4.
    - [Release notes](https://github.com/sparklemotion/nokogiri/releases)
    - [Changelog](https://github.com/sparklemotion/nokogiri/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/sparklemotion/nokogiri/compare/v1.10.3...v1.10.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Testing setup (#3)
    
    * Qa merge to deploy to prod (#1)
    
    * Intial Commit (rails new --api)
    
    * Use pg and jsonbuilder
    
    * Update to postgres for QA to build
    
    * Add auth controller and routes
    
    * Add the skeleton for a login route
    
    * Make login controller a pass-thru for the params
    
    * Bundle install and changed the gemlock
    
    * Add the initial draft of the Open API Json Schema
    
    * Update the version of the Open API schema
    
    * Change the auth controller
    
    * Fix the auth tests since the GET no longer exists

--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -2,18 +2,17 @@ require 'test_helper'
 
 class AuthControllerTest < ActionDispatch::IntegrationTest
   test "should get login" do
-    get auth_login_url
+    post auth_login_url, :params => { auth: { :username => "Test", :password => "Password"} }
     assert_response :success
   end
 
   test "should get logout" do
-    get auth_logout_url
+    post auth_logout_ur
     assert_response :success
   end
 
   test "should get oauth" do
-    get auth_oauth_url
+    post auth_oauth_url
     assert_response :success
   end
-
 end

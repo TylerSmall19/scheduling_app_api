e718ee01b955cc307148e15018366a72af0c737e
Tyler Small <tylersmall.user@gmail.com>
2019-09-05 12:08:50 -0500

    Add a before action that prevents requests from succeeding without the valid API key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::API
   include Responses
+
+  before_action -> {
+    return true if authorize params
+
+    return_not_authorized("Given API key did not match a valid key")
+  }
+
+  private
+  def authorize (params)
+    params.permit(:api_key)
+
+    # This key is temporarily stored in the ENV instead of the DB
+    (params.has_key? :api_key) && (params[:api_key] === ENV["API_KEY"])
+  end
 end

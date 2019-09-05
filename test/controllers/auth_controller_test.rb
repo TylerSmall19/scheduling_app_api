07a71389f1bff9f70e1e030373af075285831fcb
Tyler Small <tylersmall.user@gmail.com>
2019-09-05 12:07:57 -0500

    Write some tests around API key validity

--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -1,18 +1,38 @@
 require 'test_helper'
+require "minitest/autorun"
 
 class AuthControllerTest < ActionDispatch::IntegrationTest
-  test "should get login" do
-    post auth_login_url, :params => { auth: { :username => "Test", :password => "Password"} }
+  test "should return success when valid key is given" do
+    ENV["API_KEY"] = "local"
+
+    post auth_login_url + "?api_key=" + ENV["API_KEY"], :params => { auth: { :username => "Test", :password => "Password"} }
+
     assert_response :success
   end
 
-  test "should get logout" do
-    post auth_logout_ur
+  test "should give unauthorized when no key is given" do
+    post auth_login_url
+    assert_response :unauthorized
+  end
+
+  test "should give unauthorized when non-matching key is given" do
+    post auth_login_url + "?api_key=" + 'foo'
+    assert_response :unauthorized
+  end
+
+  test "should post logout" do
+    ENV["API_KEY"] = "local"
+
+    post auth_logout_url + "?api_key=" + ENV["API_KEY"]
+
     assert_response :success
   end
 
-  test "should get oauth" do
-    post auth_oauth_url
+  test "should post oauth" do
+    ENV["API_KEY"] = "local"
+
+    post auth_oauth_url + "?api_key=" + ENV["API_KEY"]
+
     assert_response :success
   end
 end

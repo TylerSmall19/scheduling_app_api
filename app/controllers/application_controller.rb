5cf298e075b923da868f428adeb169538356af86
Tyler Small <tylersmall.user@gmail.com>
2019-09-09 11:48:19 -0500

    Accept API key as a header instead of a query string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,13 @@ class ApplicationController < ActionController::API
   include Responses
 
   before_action -> {
-    return true if authorize params
-
-    return_not_authorized("Given API key did not match a valid key")
+    # return true if authorize request is successful
+    (authorize request) || return_not_authorized("Given API key did not match a valid key")
   }
 
   private
-  def authorize (params)
-    params.permit(:api_key)
-
+  def authorize (request)
     # This key is temporarily stored in the ENV instead of the DB
-    (params.has_key? :api_key) && (params[:api_key] === ENV["API_KEY"])
+    request.headers["HTTP_API_KEY"] && (request.headers["HTTP_API_KEY"] === ENV["API_KEY"])
   end
 end

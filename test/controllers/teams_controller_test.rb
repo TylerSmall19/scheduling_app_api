0348208d67e3f898c87a2294de0af9aaa9260bb5
Tyler Small <tylersmall.user@gmail.com>
2019-09-26 15:03:44 -0500

    Add some controller tests

--- test/controllers/teams_controller_test.rb
+++ test/controllers/teams_controller_test.rb
@@ -4,19 +4,28 @@ class TeamsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     ENV["API_KEY"] = "local"
 
-    post teams_url, headers: { HTTP_API_KEY: ENV["API_KEY"] }, params: { teamName: 'Test1', homeLocation: 'Test location' }
+    post teams_url, headers: { HTTP_API_KEY: ENV["API_KEY"] }, params: { teamName: 'Test1', homeLocation: 'Test location', captainId: 'testId' }
     assert_response :success
   end
 
   test "should return the created team" do
     ENV["API_KEY"] = "local"
 
-    post teams_url, headers: { HTTP_API_KEY: ENV['API_KEY'] }, params: { teamName: 'Test1', homeLocation: 'Test location' }
+    post teams_url, headers: { HTTP_API_KEY: ENV['API_KEY'] }, params: { teamName: 'Test1', homeLocation: 'Test location', captainId: 'testId' }
 
     parsed_body = JSON.parse(@response.body)
 
-    assert_equal parsed_body['teamName'], 'Test1'
-    assert_equal parsed_body['homeLocation'], 'Test location'
-    refute_nil parsed_body[:id]
+    assert_equal 'Test1', parsed_body['teamName']
+    assert_equal 'Test location', parsed_body['homeLocation']
+  end
+
+  test "should persist the created team" do
+    ENV["API_KEY"] = "local"
+
+    post teams_url, headers: { HTTP_API_KEY: ENV['API_KEY'] }, params: { teamName: 'Test1', homeLocation: 'Test location', captainId: 'testId' }
+
+    parsed_body = JSON.parse(@response.body)
+
+    refute_nil parsed_body['id']
   end
 end

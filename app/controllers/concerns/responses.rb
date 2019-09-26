5519d1af5b03184356172cdb6ab7602082a52b91
Tyler Small <tylersmall.user@gmail.com>
2019-09-26 15:01:35 -0500

    Add a failure response type with 500

--- app/controllers/concerns/responses.rb
+++ app/controllers/concerns/responses.rb
@@ -6,4 +6,8 @@ module Responses
   def return_not_authorized (msg)
     render json: { :body => msg }, status: :unauthorized
   end
+
+  def return_failure (msg)
+    render json: { :body => msg }, status: :unexpected
+  end
 end

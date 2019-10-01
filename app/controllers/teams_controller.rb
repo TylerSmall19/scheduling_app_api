db90aec72d07edbaac8928e664696f449100f7a9
Tyler Small <tylersmall.user@gmail.com>
2019-10-01 14:22:53 -0500

    Add a show route to the teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,13 +1,25 @@
 class TeamsController < ApplicationController
-  def new
-    team = Team.new(team_params)
+  def create
+    team = Team.new(new_team_params)
 
     return return_success(team) if team.save
     return_failure('Failed to save team for: ' + JSON.generate(team.errors.messages))
   end
 
+  def show
+    params.permit(:team_id)
+
+    team = Team.find params[:team_id]
+
+    if team
+      return_success(team)
+    else
+      return_failure('Something went wrong when looking for ID: ' + params[:team_id])
+    end
+  end
+
   private
-  def team_params
+  def new_team_params
     params.require(:teamName)
     params.require(:homeLocation)
     params.require(:captainId)
